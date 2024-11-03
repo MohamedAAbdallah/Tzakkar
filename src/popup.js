2ec7d8652646f25a91c87bbfc4c4a9039510d2db
Mohamed A. Abdallah <eng.mohamed.a.abdallah@gmail.com>
2024-11-03 17:29:50 +0200

    Updated popup.js functionality.

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,30 @@
+function changeIconTo(theme) {
+  const icons = {
+    green: {
+      16: "imgs/icons/Green/16.png",
+      32: "imgs/icons/Green/32.png",
+      48: "imgs/icons/Green/48.png",
+      128: "imgs/icons/Green/128.png",
+    },
+    pink: {
+      16: "imgs/icons/Pink/16.png",
+      32: "imgs/icons/Pink/32.png",
+      48: "imgs/icons/Pink/48.png",
+      128: "imgs/icons/Pink/128.png",
+    },
+  };
+
+  const selectedIcons = icons[theme];
+
+  if (selectedIcons) {
+    chrome.action.setIcon({
+      path: selectedIcons,
+    });
+  } else {
+    console.error("Invalid theme for icon change: ", theme);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const intervalInput = document.getElementById("interval");
   const themeRadioButtons = document.querySelectorAll("input[name='theme']");
@@ -13,11 +40,15 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
 
-    counterLabel.innerText = `${(parseInt(intervalInput.value, 10) / 60000).toFixed(1)} minutes`;
+    counterLabel.innerText = `${(
+      parseInt(intervalInput.value, 10) / 60000
+    ).toFixed(1)} minutes`;
   });
 
   function saveSettings() {
-    const selectedTheme = Array.from(themeRadioButtons).find(radio => radio.checked)?.value;
+    const selectedTheme = Array.from(themeRadioButtons).find(
+      (radio) => radio.checked
+    )?.value;
     const settings = {
       interval: parseInt(intervalInput.value, 10),
       theme: selectedTheme,
@@ -26,9 +57,13 @@ document.addEventListener("DOMContentLoaded", () => {
     chrome.storage.local.set({ settings }, () => {
       chrome.runtime.sendMessage({ action: "updateSettings", settings });
     });
-    counterLabel.innerText = `${(parseInt(intervalInput.value, 10) / 60000).toFixed(1)} minutes`;
+    counterLabel.innerText = `${(
+      parseInt(intervalInput.value, 10) / 60000
+    ).toFixed(1)} minutes`;
   }
 
   intervalInput.addEventListener("input", saveSettings);
-  themeRadioButtons.forEach((radio) => radio.addEventListener("change", saveSettings));
+  themeRadioButtons.forEach((radio) =>
+    radio.addEventListener("change", saveSettings)
+  );
 });
